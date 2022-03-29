fd30f4588d68d11cecfd77f45ae10ce5165d9592
ColletN <87916276+ColletN@users.noreply.github.com>
2022-03-29 15:45:52 -0700

    create 0-memeset

--- 0x07-pointers_arrays_strings/0-memeset.c
+++ 0x07-pointers_arrays_strings/0-memeset.c
@@ -1,26 +1,19 @@
-
 #include "main.h"
-
 /**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory area 's'
+ * @b: constant byte to fill
+ * @n: number of bytes to fill
+ * Return: pointer to memory area 's'
+ */
 
- * _/memset - fills memory with a constant byte
- *  * @s: is a pointer char type
- *   * @b: is a variable char type
- *    * @n: is constant int type
- *     * Return: Always 0.
- *      */
-
- char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-		s[i] = b ;
-
-	for ( ; i < n; i++)
-		s[i] = '\0';
-
-
-	return (s);
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
 
 
