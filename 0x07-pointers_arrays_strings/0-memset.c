4994edb24591b10160a08c880ce2080e7cdce90e
ColletN <87916276+ColletN@users.noreply.github.com>
2022-03-29 16:33:33 -0700

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,22 +9,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-<<<<<<< HEAD:0x07-pointers_arrays_strings/0-memset.c
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b ;
-
-	for ( ; i < n; i++)
-		s[i] = '\0';
-
-
-	return (s);
-}
-
-=======
 unsigned int i;
->>>>>>> 7292072807229978c045939cc500c7aedc3816ee:0x07-pointers_arrays_strings/0-memeset.c
 
 for (i = 0; i < n; i++)
 s[i] = b;
