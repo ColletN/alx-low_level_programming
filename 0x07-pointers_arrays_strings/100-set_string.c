9e3b4904a6e8353bb89a74b5ea714c93f2834279
ColletN <87916276+ColletN@users.noreply.github.com>
2022-03-29 19:10:20 -0700

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,10 @@
-jghdchghgdcn bhjg
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to set
+ * @to: the char to point to.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
